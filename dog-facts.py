b86ebb47b66d4667d4e9909d0a93b44985370c39
phill <phill.holbrook@gmail.com>
2021-11-18 18:46:45 -0500

    Adding Cat Fact Functionality

--- dog-facts.py
+++ dog-facts.py
@@ -16,7 +16,17 @@ async def getDogFact(ctx):
     factid = str(random.randint(1,430))
     url = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?index=" + factid
     r = requests.get(url)
-    fact = "Fact #" + factid + ": " + json.loads(r.text)[0]["fact"]
+    fact = "Dog Fact #" + factid + ": " + json.loads(r.text)[0]["fact"]
+
+    await ctx.send(fact)
+
+@bot.command(name='CatFact')
+async def getCatFact(ctx):
+
+    factid = str(random.randint(1,430)) #The fact id is just completely random lol
+    url = "https://catfact.ninja/fact"
+    r = requests.get(url)
+    fact = "Cat Fact #" + factid + ": " + json.loads(r.text)["fact"]
 
     await ctx.send(fact)
 
