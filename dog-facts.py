3c2250ecdff4fb615440aed166772004f502425f
phill <phill.holbrook@gmail.com>
2021-11-18 21:29:52 -0500

    Adding commands

--- dog-facts.py
+++ dog-facts.py
@@ -7,8 +7,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-bot = commands.Bot(command_prefix='/', case_insensitive=True)
+bot = commands.Bot(command_prefix='!', case_insensitive=True)
 token = os.environ.get('TOKEN')
+
+@bot.command(name='help')
+async def getHelp(ctx):
+
+    resp = 'Current supported commands:\n - `!dogfact`: Gets a random fact about dogs.\n - `!catfact`: Gets a random fact about cats.\n - `!help`: Shows this message.\n - `!about`: Shows information about me.'
+
+    await ctx.send(resp)
+
+@bot.command(name='about')
+async def getAbout(ctx):
+
+    resp = 'Hi! I\'m DogFacts. My creator made me as part of a side project to learn about Python, GitHub Actions, and AWS CodePipeline. To see what commands I currently support, use `!help`. \n\n See my source code here: https://github.com/phill-holbrook/dog-facts'
+
+    await ctx.send(resp)
 
 @bot.command(name='DogFact')
 async def getDogFact(ctx):
