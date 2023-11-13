5e01096ae6f493d0076533d5929a2b6d22efcdce
Phill Holbrook <phill.holbrook@gmail.com>
2023-11-12 22:22:00 -0500

    command names must be lowercase

--- dog-facts.py
+++ dog-facts.py
@@ -15,19 +15,19 @@ bot = commands.Bot(case_insensitive=True, intents=intents)
 token = os.environ.get('TOKEN')
 
 #@bot.remove_command('help')
-@bot.slash_command(name='help', description='Get help with DogFacts!')
-async def getHelp(ctx):
+# @bot.slash_command(name='help', description='Get help with DogFacts!')
+# async def getHelp(ctx):
 
-    resp = 'Current supported commands:\n - `/dogfact`: Gets a random fact about dogs.\n - `/catfact`: Gets a random fact about cats.\n - `/help`: Shows this message.\n - `/about`: Shows information about me.'
+#     resp = 'Current supported commands:\n - `/dogfact`: Gets a random fact about dogs.\n - `/catfact`: Gets a random fact about cats.\n - `/help`: Shows this message.\n - `/about`: Shows information about me.'
 
-    await ctx.send(resp)
+#     await ctx.send(resp)
 
 @bot.slash_command(name='about', description='Get information about DogFacts!')
 async def get_about(ctx):
     resp = 'Hi! I\'m **DogFacts**. My creator made me as part of a side project to learn about Python, GitHub Actions, and AWS CodePipeline. To see what commands I currently support, use `/help`.\n\nSee my source code here: https://github.com/phill-holbrook/dog-facts'
     await ctx.send(resp)
 
-@bot.slash_command(name='DogFact', description='Get a DogFact!')
+@bot.slash_command(name='dogfact', description='Get a DogFact!')
 async def getDogFact(ctx):
 
     factid = str(random.randint(1,1000)) #The fact id is just completely random lol
@@ -37,7 +37,7 @@ async def getDogFact(ctx):
 
     await ctx.send(fact)
 
-@bot.slash_command(name='CatFact', description='Get a CatFact!')
+@bot.slash_command(name='catfact', description='Get a CatFact!')
 async def getCatFact(ctx):
 
     factid = str(random.randint(1,1000)) #The fact id is just completely random lol
